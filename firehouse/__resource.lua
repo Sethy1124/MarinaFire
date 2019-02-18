065f59a1db7b047029bbb7bfb77578d8ca82e67b
Sethy1124 <47252832+Sethy1124@users.noreply.github.com>
2019-02-18 14:02:25 -0600

    Add files via upload

---
+++ firehouse/__resource.lua
@@ -0,0 +1,5 @@
+-- This is an addon map created by Seth S.
+-- If you downloaded this from anywhere besides the FiveM forums please let me know and do not use this.
+resource_manifest_version '77731fab-63ca-442c-a67b-abc70f28dfa5' 
+
+this_is_a_map 'yes'
